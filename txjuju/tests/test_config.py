0ed51c8f80bff1e72e2e99d731109632b3f56466
Eric Snow <ericsnowcurrently@gmail.com>
2016-09-23 00:25:40 +0000

    Frame out the unit tests.

---
+++ txjuju/tests/test_config.py
@@ -0,0 +1,33 @@
+# Copyright 2016 Canonical Limited.  All rights reserved.
+
+from datetime import timedelta
+import unittest
+
+import yaml
+
+from txjuju.config import (
+    Config, ControllerConfig, CloudConfig, BootstrapConfig)
+
+
+class ConfigTest(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+
+class ControllerConfigTest(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+
+class CloudConfigTest(unittest.TestCase):
+
+    def test_(self):
+        pass
+
+
+class BootstrapConfigTest(unittest.TestCase):
+
+    def test_(self):
+        pass
