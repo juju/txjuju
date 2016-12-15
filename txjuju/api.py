1a0b2b6a015cf4f97fa8e622fa16a9c7162d2302
Chad Smith <chad.smith@canonical.com>
2016-12-15 11:33:26 -0700

    add unit tests and _parseSetModelConfigResult to handle raising api errors

--- txjuju/api.py
+++ txjuju/api.py
@@ -191,11 +191,11 @@ class Juju2APIClient(object):
                                      params=params)
         return deferred.addCallback(self._parseModelInfo)
 
-    def setModelConfig(self, key_name, value):
+    def setModelConfig(self, keyname, value):
         """Set model config values."""
         params = {"config": {keyname: value}}
         deferred = self._sendRequest("ModelConfig", "ModelSet", params=params)
-        return deferred.addCallback(self._parseErrorResults)
+        return deferred.addCallback(self._parseSetModelConfigResult)
         
     def cloud(self, cloudtag):
         """Return information about the model's cloud.
@@ -555,6 +555,11 @@ class Juju2APIClient(object):
         # This is not a non-local IPv4 address, let's check if it's a
         # fake-juju one instead.
         return network == "dummy-provider-network" and type_ == "hostname"
+
+    def _parseSetModelConfigResult(self, response):
+        """Parse setModelConfig response for any errors."""
+        if response.get("results"):
+            self._parseErrorResults(response)
 
     def _parseModelInfo(self, response):
         """Parse the response of a modelInfo request."""
