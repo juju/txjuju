8c2a039a0adb2fe39fc4da3ec4b13fdc2ee71d8e
Free Ekanayaka <free@ekanayaka.io>
2017-02-09 14:43:46 +0100

    Re-enable integration tests, using the new fakejuju fixture (#46)
    Integration tests exercising txjuju with fake-juju where disabled at
    some point in the past, probably during the migration to Juju 2 and/or
    to GitHub/Travis.
    
    This branch restores them, with the necessary modifications to run
    against fake-juju 2.0.2.
    
    Since Juju 2 has no direct equivalent of the "model info" API, that
    one test was dropped.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,11 @@
 # Copyright 2016 Canonical Limited.  All rights reserved.
+import os
+
+from testresources import OptimisingTestSuite
+
+
+def load_tests(loader, standard_tests, pattern):
+    this_dir = os.path.dirname(__file__)
+    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
+    standard_tests.addTests(package_tests)
+    return OptimisingTestSuite(standard_tests)
